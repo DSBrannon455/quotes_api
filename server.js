7cf8179d58a68d072f8bec7725fe607437ed59f3
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-05-23 18:31:21 -0500

    Added CORS to the backend

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const express = require('express')
 const app = express()
 const PORT = 3003
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 
 
 // MIDDLEWARE
 // this will tell the server to parse the JSON data, and create the req.body object.
 app.use(express.json())
 
+// Setup Cors middleware
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS')) 
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 
 
